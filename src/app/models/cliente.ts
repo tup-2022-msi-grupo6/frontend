578c4ed090baa73f66dfea96794a51b91f996bb6
Tomás Martínez <111818@tecnicatura.frc.utn.edu.ar>
2022-11-01 18:36:45 -0300

    carga de listado de clientes y login finalizado

--- src/app/models/cliente.ts
+++ src/app/models/cliente.ts
@@ -1,4 +1,10 @@
 export interface Cliente {
   id: number;
   nombre: string;
+  // apellido: string;
+  // direccion: string;
+  // telefono: number;
+  // dni: number;
+  // email: string;
+  // barrio: string;
 }
